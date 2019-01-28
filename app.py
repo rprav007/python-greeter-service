cb4a152c236025ad789375b8d739925fd0b25b85
cricci82 <chris.ricci82@gmail.com>
2019-01-28 15:36:07 -0500

    adding greeting list

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 #!flask/bin/python
 from flask import Flask
 from flask_prometheus import monitor
+import random
 
 app = Flask(__name__)
 
+f = open('greetings.txt', 'r')
+names = f.readlines()
+f.close()
+
 @app.route('/')
 def index():
-    return "Hello"
+    return random.choice(names).strip()
 
 if __name__ == '__main__':
     monitor(app, port=8000)
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=8080)
